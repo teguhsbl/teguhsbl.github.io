9756d7a90d74d34f19a594e9b89e5a75a73268dd
teguhsbl <teguhsabila4@gmail.com>
2023-08-11 15:01:45 +0700

    first commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,24 @@
+// .vitepress/config.js
+export default {
+  title: 'Teguh Sabila',
+  description: 'Art and web programming',
+
+  themeConfig: {
+    logo: "/me.jpg",
+    siteTitle: "Teguh Sabila",
+    nav: [
+    { text: "Project", link: "/our-project"},
+    { text: "Notes", link: "/notes"},
+    { text: "About", link: "/about" },
+    { text: "Contact", link: "/contact" },
+    ],
+    socialLinks: [
+    { icon: "github", link: "https://github.com/teguhsbl" },
+    { icon: "facebook", link: "https://fb.me/sabilateguh" },
+    ],
+    footer: {
+   message: "This Portofolio build with Vitepress.",
+   copyright: "Copyright © 2023-present Teguh Sabila",
+ },
+  },
+}
